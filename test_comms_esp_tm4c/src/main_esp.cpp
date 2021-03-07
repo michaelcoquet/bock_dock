95935694ae5f09d2ac8ff5463613b5c0c05c098d
Michael Coquet <m.coquet2@gmail.com>
2021-03-06 19:10:19 -0600

    fix deprecated function warning, and adds some datasheets

--- test_comms_esp_tm4c/src/main_esp.cpp
+++ test_comms_esp_tm4c/src/main_esp.cpp
@@ -1,19 +1,19 @@
 #include <Arduino.h>
 
 #include <ESP8266WiFi.h>
+
 #include <ESP8266WebServer.h>
+
 #include <ESP8266HTTPClient.h>
+
 #include <DNSServer.h>
+
 #include <WiFiManager.h>         // https://github.com/tzapu/WiFiManager
+ // #include <TaskScheduler.h>
 
-
-const String api_key = "uknNrncEDa4KlhypVm3fCxtYqHRyg453";  // blynk api authentication code
+const String api_key = "uknNrncEDa4KlhypVm3fCxtYqHRyg453"; // blynk api authentication code
 const String server_url = "http://blynk-cloud.com/";
 const String pin_name = "V0";
-
-
-// // Set web server port number to 80
-// WiFiServer server(80);
 
 // Variable to store the HTTP request
 String header;
@@ -24,53 +24,59 @@ String green_led_state = "off";
 String blue_led_state = "off";
 
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  // WiFiManager
-  // Local intialization. Once its business is done, there is no need to keep it around
-  WiFiManager wifiManager;
-  
-  // Uncomment and run it once, if you want to erase all the stored information
-  //wifiManager.resetSettings();
-  
-  // set custom ip for portal
-  //wifiManager.setAPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+    // WiFiManager
+    // Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wifiManager;
 
-  // fetches ssid and pass from eeprom and tries to connect
-  // if it does not connect it starts an access point with the specified name
-  // here  "AutoConnectAP"
-  // and goes into a blocking loop awaiting configuration
-  wifiManager.autoConnect("Kegerator Connect");
-  // or use this for auto generated name ESP + ChipID
-  //wifiManager.autoConnect();
-  // if you get here you have connected to the WiFi
-//  Serial.println("Connected.");
-  
-  // server.begin();
-  ESP.reset();
-//  wifiManager.reboot();
+    // Uncomment and run it once, if you want to erase all the stored information
+    //wifiManager.resetSettings();
+
+    // set custom ip for portal
+    //wifiManager.setAPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+    // fetches ssid and pass from eeprom and tries to connect
+    // if it does not connect it starts an access point with the specified name
+    // here  "AutoConnectAP"
+    // and goes into a blocking loop awaiting configuration
+    wifiManager.autoConnect("Kegerator Connect");
+    // or use this for auto generated name ESP + ChipID
+    //wifiManager.autoConnect();
+    // if you get here you have connected to the WiFi
+    //  Serial.println("Connected.");
+
+    // server.begin();
+    // ESP.reset();
+    //  wifiManager.reboot();
 }
 
 void loop() {
-    if(WiFi.status() == WL_CONNECTED) {
-      HTTPClient http;
+  if(WiFi.status() == WL_CONNECTED) {
+    WiFiClient client;
+    HTTPClient http;
 
-      String path = server_url + api_key + "/update/" +  pin_name + "?value=" + String(random(1,420));
+    String path = server_url + api_key + "/update/" + pin_name + "?value=" + String(random(1, 420));
 
-      Serial.println("Trying to connect to path: " + path);
+    Serial.println("Trying to connect to path: " + path);
 
-      http.begin(path.c_str());
-
+    if(http.begin(client, path.c_str()))
+    {            
       int httpResponseCode = http.GET();
 
       Serial.println("response code received = " + String(httpResponseCode));
-
-      http.end();
     }
-    Serial.println("Waiting...");
-  
-    // wait 30 seconds before sending another value
-    delay(10000);
+    else
+    {
+      Serial.println("ERROR: http.begin()");
+    }
+
+    http.end();
+  }
+  Serial.println("Waiting...");
+
+  // wait 30 seconds before sending another value
+  delay(10000);
 }
 
 // void loop() {
@@ -94,7 +100,7 @@ void loop() {
 //             client.println("Content-type:text/html");
 //             client.println("Connection: close");
 //             client.println();
-            
+
 //             // turns the GPIOs on and off
 //             if (header.indexOf("GET /0/1") >= 0) {
 //               Serial.println("01");
@@ -115,7 +121,7 @@ void loop() {
 //               Serial.println("10");
 //               green_led_state = "off";
 //             }
-            
+
 //             // Display the HTML web page
 //             client.println("<!DOCTYPE html><html>");
 //             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
@@ -126,10 +132,10 @@ void loop() {
 //             client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
 //             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
 //             client.println(".button2 {background-color: #77878A;}</style></head>");
-            
+
 //             // Web Page Heading
 //             client.println("<body><h1>Kegerator Monitor</h1>");
-            
+
 //             // Display current state, and ON/OFF buttons for Red LED  
 //             client.println("<p>Red LED - State " + red_led_state + "</p>");
 //             // If the red_led_state is off, it displays the ON button       
@@ -138,7 +144,7 @@ void loop() {
 //             } else {
 //               client.println("<p><a href=\"/0/0\"><button class=\"button button2\">OFF</button></a></p>");
 //             } 
-               
+
 //             // Display current state, and ON/OFF buttons for Blue LED 
 //             client.println("<p>Blue LED - State " + blue_led_state + "</p>");
 //             // If the blue_led_state is off, it displays the ON button       
@@ -157,7 +163,7 @@ void loop() {
 //               client.println("<p><a href=\"/1/0\"><button class=\"button button2\">OFF</button></a></p>");
 //             }
 //             client.println("</body></html>");
-            
+
 //             // The HTTP response ends with another blank line
 //             client.println();
 //             // Break out of the while loop
